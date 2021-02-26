77c5f882a81c89118b704b09c8c3593b7bd288af
einars sviksa <einars24@gmail.com>
2021-02-26 18:59:07 +0200

    eksamens

--- eksāmens_dzejolis.py
+++ eksāmens_dzejolis.py
@@ -1,8 +1,38 @@
-#dzejolis
-
 #print("Mācies, kamēr gudrs tiec,"
   #  "Visu labi vērā liec.",
   #  "Dzīve gudra, viltīga", 
-   # "Gudrība tur derīga!")
+  #  "Gudrība tur derīga!")
+
+#2 
+import sys
+print ("Python version")
+print(sys.version)
+print("Version info.")
+print(sys.version_info)
+
+#3 
+from math import pi
+r = float(input) ("Ievadi rinķa radiusu: "))
+print("Rinka laukums" + str(r) + "ir" + str(pi * r**2 ))
+
+# 4
 a = input()
 print(a[::-1])
+
+# 5 
+a = 0
+for i in range(0, 10):
+    n = int(input())
+    a = a + n
+print(a)
+
+
+#6
+saraksts = ("aboli", "bumbieri", "rieksti", "banani", "kartupeli")
+print(saraksts[0], saraksts[4])
+
+#8
+a = int(input)
+    if a 
+#pedejais
+print("Man viss izdodas!")
